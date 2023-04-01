998b16dfb83c39229bce22abbb51d5f937c00266
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-04-01 14:51:21 -0400

    handle images with solid background still have issues with black on black images ("penguin.png")

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 from rembg import remove
 from FaceDetectionInfo import bottom_value_decider
 from FaceCropper import crop
+import numpy as np
 
 # Remove background
-input_path = 'penguin.png'
-output_path = 'removed_bg.png'
+input_path = 'images/luigi.png'
+output_path = 'output/removed_bg.png'
 
-input = Image.open(input_path)
-output = remove(input)
+
+def shift_contrast(im: Image, color = None):
+    data = np.array(im)
+    print(data.shape)
+    if color is None:
+        r1, g1, b1 = 0, 0, 0  # Original value
+        pixels = set([i for i in im.getdata()])
+        all_colors = list(np.ndindex((255, 255, 255)))
+        non_colors = []
+        i = len(all_colors)-1
+        while len(non_colors) < 10 and i > 0:
+            if all_colors[i] not in pixels:
+                non_colors.append(all_colors[i])
+            i -= 1
+        color = non_colors[0]  # find brightest color that is not in the image
+        r2, g2, b2 = color
+    else:
+        r1, g1, b1 = color
+        r2, g2, b2 = 0, 0, 0
+
+    red, green, blue = data[:, :, 0], data[:, :, 1], data[:, :, 2]
+    mask = (red == r1) & (green == g1) & (blue == b1)
+    data[:, :, :3][mask] = (r2, g2, b2)
+
+    return Image.fromarray(data), color
+
+def transfer_to_blank_canvas(im: Image, model: Image):
+    model_outline = model.getbbox()
+    clear_image = im.crop(model_outline)
+    img = Image.new('RGBA', model.size, (255, 0, 0, 0))
+    img.paste(clear_image, (0, 0), clear_image)
+    return img
+
+input = Image.open(input_path).convert('RGBA')
+intermediate, color = shift_contrast(input)
+intermediate.save("output/shifted.png")
+intermediate = intermediate.filter(ImageFilter.FIND_EDGES)
+intermediate.save("output/outline.png")
+ImageDraw.floodfill(intermediate, xy=(0, 0), value=color)
+intermediate.paste(input, (0, 0), input)
+output = remove(intermediate)
+output, _ = shift_contrast(output, color)
 output.save(output_path)
 
 image_path = output_path
@@ -31,8 +72,10 @@ centerpiece_x = 0
 centerpiece_y = 0
 
 # Resize and paste centerpiece image onto thumbnail image
-centerpiece_image = centerpiece_image.resize((centerpiece_width, centerpiece_height))
-thumbnail_image.paste(centerpiece_image, (centerpiece_x, centerpiece_y), centerpiece_image)
+centerpiece_image = centerpiece_image.resize(
+    (centerpiece_width, centerpiece_height))
+thumbnail_image.paste(centerpiece_image, (centerpiece_x,
+                      centerpiece_y), centerpiece_image)
 
 # Determine text box size and position
 text_x = int(thumbnail_image.width / 2)
@@ -42,7 +85,8 @@ text_height = thumbnail_image.height
 
 # Create new draw object and set font
 draw = ImageDraw.Draw(thumbnail_image)
-font = ImageFont.truetype('/System/Library/Fonts/Supplemental/arial.ttf', size=36)
+font = ImageFont.truetype(
+    '/System/Library/Fonts/Supplemental/arial.ttf', size=36)
 
 text_lines = "Hi I'm Mario".split('\n')
 
--- test_paddleclas.py
+++
@@ -1,6 +0,0 @@
-from paddleclas import PaddleClas
-
-clas = PaddleClas(model_name='PP-ShiTuV2')
-infer_imgs='images/fairy.png'
-result=clas.predict(infer_imgs)
-print(next(result))
