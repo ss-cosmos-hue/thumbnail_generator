6bc6a578072dfa0bf6db3953c18eea75c21e972d
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-04-01 15:43:29 -0400

    add frontend

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  document.getElementById("frm1").submit();
+}
+function WordCount(str) {
+  var totalSoFar = 0;
+  for (var i = 0; i < WordCount.length; i++)
+    if (str(i) === " ") {
+      totalSoFar = +1;
+    }
+  totalsoFar += 1;
+}
+function validateForm() {
+  let x = document.forms["frm1"]["words"].value;
+  if (x < 1 || x > 3) {
+    alert("Please enter a phrase between 1-3 words");
+    return false;
+  }
+}
